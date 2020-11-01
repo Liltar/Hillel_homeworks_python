3cb9ca510f06a2c50854e82510fefebef696bfcd
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-01 23:43:16 +0200

    1

---
+++ Lesson_1/Task_5.py
@@ -0,0 +1,25 @@
+#5. Написать программу, которая выводит в консоль таблицу Escape-последовательностей:
+# Escape sequences
+#
+#  \a     Bell (alert)
+#
+#  \b     Backspace
+#
+#  \n     New line
+#
+#   \t     Horizontal tab
+#
+#   \\     Backslash \
+#
+#   \”     Double quotation mark “
+#
+#   \’     Single quotation mark ‘
+
+print('Escape sequences:\n\n',
+      '\\a     Bell (alert)\n',
+      '\\b     Backspace\n',
+      '\\n     New line\n',
+      '\\t     Horizontal tab\n',
+      '\\\     Backslash \\\n',
+      '\\”     Double quotation mark “\n',
+      '\\’     Single quotation mark ‘\n')
