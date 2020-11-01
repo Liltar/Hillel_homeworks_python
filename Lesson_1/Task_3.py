26effd965c02ae2b181f3986caea057f15241a26
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-01 21:59:47 +0200

    1

---
+++ Lesson_1/Task_3.py
@@ -0,0 +1,12 @@
+# 3a - show (print) the Zen of python
+import this
+#
+# 3b - отображение комикса о python
+import antigravity
+#
+# 3c - выполнение модуля __hello__, выводит Hello World!
+import __hello__
+#
+# 3d -  намёк, что фигурных скобок в языке не будет
+# в __future__ находятся возможности будущих версий
+from __future__ import braces
