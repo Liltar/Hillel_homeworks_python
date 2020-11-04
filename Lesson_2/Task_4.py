86a818d10aa78799d381307f815aca30cc92d40d
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-04 23:40:08 +0200

    1

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -5,14 +5,18 @@
 
 some_input = input('Enter number: ')
 try:
+    # if input is a int
     val = int(some_input)
     if (int(some_input) % 4) == 0 and int(some_input) % 100 != 100 and int(some_input) % 400:
         print(some_input, 'is a leap year')
     else:
         print(some_input, 'is a common year')
+# if input is not an int
 except ValueError:
+    # if input is a float
     try:
         val = float(some_input)
         print('Input is a float number')
+    # if input is a string
     except ValueError:
         print('Input is a string')
