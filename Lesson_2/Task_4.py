1f22a0c9489dc91132ee8af3bde83055b0f30776
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-04 23:14:02 +0200

    1

--- Lesson_2/Task_4.py
+++ Lesson_2/Task_4.py
@@ -4,7 +4,15 @@
 # год является високосным, если его номер кратен 4, но не кратен 100, а также если он кратен 400.
 
 some_input = input('Enter number: ')
-if (int(some_input) % 4) == 0 and int(some_input) % 100 != 100 and int(some_input) % 400:
-    print(some_input, 'is a leap year')
-else:
-    print(some_input, 'is a common year')
+try:
+    val = int(some_input)
+    if (int(some_input) % 4) == 0 and int(some_input) % 100 != 100 and int(some_input) % 400:
+        print(some_input, 'is a leap year')
+    else:
+        print(some_input, 'is a common year')
+except ValueError:
+    try:
+        val = float(some_input)
+        print('Input is a float number')
+    except ValueError:
+        print('Input is a string')
