16de2ea814d034cc576cb88864363a9c59284347
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-05 11:31:59 +0200

    init commit

---
+++ Lesson_2/Task_7.py
@@ -0,0 +1,11 @@
+# 7. ** Написать программу "Показать звезды". На вход принимает обычное целое число,
+# должно напечатать количество звезд:
+#
+#      Пример:
+#
+#      Input number of stars: 8
+#      ********
+
+some_input = input('Input number of stars: ')
+stars_number = int(some_input)
+print('*' * stars_number)
