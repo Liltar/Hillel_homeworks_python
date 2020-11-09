f8b4a1eea91e5708d4b3e30b8386d646830ff8c7
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-09 17:21:04 +0200

    init commit

--- Lesson_3/task_5.py
+++ Lesson_3/task_5.py
@@ -5,9 +5,23 @@
 import random
 programm_number = random.randint(0, 10)
 attempts = 0
-user_guess = (input('Enter a number from 1 to 10: '))
 while attempts < 3:
+    attempts = 1
+    user_guess = (input('Enter a number from 1 to 10: '))
     if int(user_guess) == programm_number:
         print('You won!')
     else:
-        print('You lose!')
+        print('Try again!')
+        attempts = 2
+        user_guess = (input('Enter a number from 1 to 10: '))
+        if int(user_guess) == programm_number:
+            print('You won!')
+        else:
+            print('Try again!')
+            attempts = 3
+            user_guess = (input('Enter a number from 1 to 10: '))
+            if int(user_guess) == programm_number:
+                print('You won!')
+            else:
+                print('You lose!')
+print(f'The answer is {programm_number}')
