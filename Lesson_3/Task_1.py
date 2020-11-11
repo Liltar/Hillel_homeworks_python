80581bb03c816409c41ea2e33b690a4770c32e52
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-11 14:15:29 +0200

    init commit

--- Lesson_3/Task_1.py
+++ Lesson_3/Task_1.py
@@ -1,6 +1,3 @@
 # Пользователь вводит трехзначное число. Найдите сумму его цифр.
-some_input = (input('Enter a 3-digit number: '))
-sum_of_digits = 0
-for i in some_input:
-    sum_of_digits += int(i)
-print(sum_of_digits)
+some_input = sum([int(i) for i in input('Enter a 3-digit number: ')])
+print(some_input)
