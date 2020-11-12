71f1f68f951ce7ab8e5f7d8bd0f014a95f6163bb
taras_kolomyia <Freedomforever94>
2020-11-12 18:46:11 +0200

    init commit

--- lesson_4/task_1.py
+++ lesson_4/task_1.py
@@ -1,9 +1,24 @@
-# 1. В первый день спортсмен пробежал `x` километров,
-# а затем он каждый день увеличивал пробег на 10% от предыдущего значения.
-# По данному числу `y` определите номер дня, на который пробег спортсмена составит не менее `y` километров.
-#
-# Программа получает на вход числа `x` и `y` и должна вывести одно число - номер дня.
+# some_input = sum([int(i) for i in input('Enter a 3-digit number: ')])
+# print(some_input)
 
-def distance(x):
-    while True:
+# 5. Пользователь вводит число от 1 до 10, программа генерирует рандомное число от 1 до 10,
+# если числа равны напечатать 'You won!' если нет - 'You lose!'.
+# Дать пользователю три попытки ;)
 
+import random
+programm_number = random.randint(0, 10)
+attempts = 0
+while attempts < 3:
+    user_guess = random.randint(0, 10)
+    attempts += 1
+    if int(user_guess) == programm_number:
+        print('You won!')
+        break
+    else:
+        print('Try again!')
+    attempts += 1
+else:
+    print('You lose!')
+print(f'The answer is {programm_number}')
+
+print(globals())
