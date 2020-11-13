0901e0581b5688375c133890a10dcd000ab7214d
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-13 18:38:26 +0200

    init commit

--- lesson_4/task_1.py
+++ lesson_4/task_1.py
@@ -5,20 +5,27 @@
 # если числа равны напечатать 'You won!' если нет - 'You lose!'.
 # Дать пользователю три попытки ;)
 
-import random
-programm_number = random.randint(0, 10)
-attempts = 0
-while attempts < 3:
-    user_guess = random.randint(0, 10)
-    attempts += 1
-    if int(user_guess) == programm_number:
-        print('You won!')
-        break
-    else:
-        print('Try again!')
-    attempts += 1
-else:
-    print('You lose!')
-print(f'The answer is {programm_number}')
-
-print(globals())
+# import random
+# programm_number = random.randint(0, 10)
+# attempts = 0
+# while attempts < 3:
+#     user_guess = random.randint(0, 10)
+#     attempts += 1
+#     if int(user_guess) == programm_number:
+#         print('You won!')
+#         break
+#     else:
+#         print('Try again!')
+#     attempts += 1
+# else:
+#     print('You lose!')
+# print(f'The answer is {programm_number}')
+#
+# print(globals())
+#
+first_day_dist = int(input('Enter a first day distance: '))
+last_day_dist = int(input('Enter an intended distance: '))
+i = 1
+while first_day_dist < last_day_dist:
+    first_day_dist *= 1.1
+    i += 1
