55574c83f461c3cd2702a794b2e2a17499bb1ad7
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-15 15:02:06 +0200

    init commit

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,13 +1,27 @@
 # 3. Даны два целых числа `A` и `В`.
 # Выведите все числа от `A` до `B` включительно, в порядке возрастания, если `A < B`,
 # или в порядке убывания в противном случае.
+# var 1
+# if first_number < second_number:
+#     for i in range(first_number, second_number + 1):
+#         print(i)
+# elif first_number > second_number:
+#     for i in range(first_number, second_number - 1, - 1):
+#         print(i)
+# else:
+#     print('No way!')
+
+# var 2
 first_number = int(input('Enter a first number: '))
 second_number = int(input('Enter a second number: '))
-if first_number < second_number:
-    for i in range(first_number, second_number + 1):
-        print(i)
-elif first_number > second_number:
-    for i in range(first_number, second_number - 1, - 1):
-        print(i)
-else:
-    print('No way!')
+def number_list_1():
+    if first_number < second_number:
+        for i in range(first_number, second_number + 1):
+            return i
+    elif first_number > second_number:
+        for i in range(first_number, second_number - 1, - 1):
+            return i
+    else:
+        print('No way')
+
+print(number_list_1())
