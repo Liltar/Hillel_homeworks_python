0ad8f6f3f5220b1b192c131ea01411b336daba47
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-15 12:54:12 +0200

    init commit

--- lesson_4/task_2.py
+++ lesson_4/task_2.py
@@ -25,16 +25,43 @@ while True:
     if next_val == 0:
         break
     list_to_collect.append(next_val)
-# - количество введённых чисел (завершающий 0 не считается)
-numbers_count = len(list_to_collect)
-# - их сумму
-numbers_sum = sum(list_to_collect)
-# - произведение
-numbers_multiply = 1
-for i in list_to_collect:
-    numbers_multiply *= i
-# - среднее арифметическое (не считая завершающего числа 0)
-numbers_avg = numbers_multiply / numbers_count
-# - определить значение и порядковый номер наибольшего элемента последовательности
-for i in list_to_collect:
-    if
+
+
+def calculate(list_to_collect):
+    count_numbers = 0
+    sum_numbers = 0
+    multiply_numbers = 1
+    max_number = 0
+    max_number_index = 0
+    even_numbers = 0
+    odd_numbers = 0
+    second_max_number = 0
+    last_element = 0
+    max_elements = 0
+    for i, value in enumerate(list_to_collect):
+        count_numbers += 1
+        sum_numbers += value
+        multiply_numbers *= value
+        if value > max_number:
+            max_number = value
+            max_number_index = i
+        if value < max_number:
+            second_max_number = value
+        if value % 2 == 0:
+            even_numbers += 1
+        if value + last_element == max_number:
+            max_elements += 1
+        else:
+            odd_numbers += 1
+        last_element = value
+    numbers_avg = multiply_numbers / count_numbers
+    return (count_numbers, sum_numbers, multiply_numbers, numbers_avg,
+            max_number, max_number_index, even_numbers, odd_numbers,
+            second_max_number, max_elements)
+
+print(calculate(list_to_collect))
+
+# # - определить значение и порядковый номер наибольшего элемента последовательности
+# for value in list_to_collect:
+#     if
+
