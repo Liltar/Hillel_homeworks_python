63c075e123046d82572e0bcf242120b420818fbc
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-16 17:46:49 +0200

    init commit

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -4,10 +4,14 @@
 # (Простые числа - те которые делятся без остатка только на само себя или 1,
 # например 2, 3, 5, 7, 11...)
 input_number = int(input('Enter a number: '))
+
+
 def is_prime(input_number):
-    for i in range(input_number + 1):
-        if input_number % i:
-            return True
-        else:
+    for i in range(2, input_number):
+        if (input_number % i) == 0:
             return False
-is_prime(input_number)
+        else:
+            return True
+
+
+print(is_prime(input_number))
