1b4e16eff69abefc647ba742b5f9a6c3a208dd56
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-16 17:55:09 +0200

    init commit

---
+++ lesson_5/task_2.py
@@ -0,0 +1,5 @@
+# 2. Написать программу которая вернет количество введенных пользователем слов и общее число символов.
+some_str = input('Enter a string')
+
+
+def str_words_and_symbols_quantity(some_str):
