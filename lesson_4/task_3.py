9bd9f1d66157f16656633dc4911dadb23edcb66c
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-17 14:32:33 +0200

    init commit

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,7 +1,22 @@
 # 3. Даны два целых числа `A` и `В`.
 # Выведите все числа от `A` до `B` включительно, в порядке возрастания, если `A < B`,
 # или в порядке убывания в противном случае.
-# var 1
+
+first_number = int(input('Enter a first number: '))
+second_number = int(input('Enter a second number: '))
+
+
+def number_list_2(*args):
+    step = 1
+    if first_number > second_number:
+        step = -1
+    for i in range(first_number, second_number + step, step):
+        print(i)
+
+
+number_list_2(first_number, second_number)
+
+# var 2
 # if first_number < second_number:
 #     for i in range(first_number, second_number + 1):
 #         print(i)
@@ -11,17 +26,13 @@
 # else:
 #     print('No way!')
 
-# var 2
-first_number = int(input('Enter a first number: '))
-second_number = int(input('Enter a second number: '))
-def number_list_1():
-    if first_number < second_number:
-        for i in range(first_number, second_number + 1):
-            print(i)
-    elif first_number > second_number:
-        for i in range(first_number, second_number - 1, - 1):
-            print(i)
-    else:
-        print('No way')
-
-number_list_1()
+# var 3
+# def number_list_1():
+#     if first_number < second_number:
+#         for i in range(first_number, second_number + 1):
+#             print(i)
+#     elif first_number > second_number:
+#         for i in range(first_number, second_number - 1, - 1):
+#             print(i)
+#     else:
+#         print('No way')
