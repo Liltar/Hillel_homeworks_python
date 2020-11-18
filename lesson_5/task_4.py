55946817dd6d48707385ebb3aeeef954385b43e1
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-18 23:59:07 +0200

    init commit

--- lesson_5/task_4.py
+++ lesson_5/task_4.py
@@ -1,7 +1,18 @@
 # 4. Дан список случайных целых чисел.
 # Замените все нечетные числа списка нулями.
 # И выведете их количество.
-
 some_list = list(range(0, 10))
-for i in some_list:
-    if i % 2 == 0:
+
+
+def odd_numbers_count_and_replace(arg: list):
+    odd_numbers_count = 0
+    for index, item in enumerate(arg):
+        if item % 2 != 0:
+            odd_numbers_count += 1
+            arg[index] = 0
+        else:
+            pass
+    return arg, odd_numbers_count
+
+
+print(odd_numbers_count_and_replace(some_list))
