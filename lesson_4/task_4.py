c0d2ec5b3fd1da1d9cf817ec3518abfe3a6bd7fa
Liltar <60432201+Liltar@users.noreply.github.com>
2020-11-18 12:49:41 +0200

    init commit

--- lesson_4/task_4.py
+++ lesson_4/task_4.py
@@ -12,8 +12,6 @@
 #    1234
 #
 #    12345
-
-
 enter_number = input('Enter a number: ')
 for i in range(int(enter_number) + 1):
     list_1 = ''.join([str(num) for num in range(1, int(enter_number)+ 1)])
