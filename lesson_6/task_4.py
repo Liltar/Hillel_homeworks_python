7c0764ecb535db362d588ad32043f77fa26a8359
liltar <Freedomforever94>
2020-12-02 13:09:04 +0300

    init commit

--- lesson_6/task_4.py
+++ lesson_6/task_4.py
@@ -1,5 +1,20 @@
+# Во вложении есть json файл. Написать программу которая прочитает его
+# и посчитает общую длительность всех треков в альбоме.
+#
+# Базовый кейс - вернет количество секунд.
+#
+#    * доп. усложнение вернуть в виде строки часы:минуты:секунды,
+#    прим. '0:41:39' (datetime.timedelta)
+
+
 import json
-f = open('acdc.json', 'r')
-json_text_file = f.read()
-print(json_text_file)
-# with open('acdc.json')
+with open('acdc.json', 'r') as f:
+    json_text_file_acdc = f.read()
+    acdc = json.loads(json_text_file_acdc)
+    i = 0
+    track_duration_1 = (acdc['album']['tracks']['track'][i]['duration'])
+    track_durations = [value for value in acdc['album']['tracks']['track'][i]['duration']]
+    print(track_duration_1)
+
+
+
