ef32e31f979769fec8b3a29f663527344292e6ef
liltar <Freedomforever94>
2020-12-03 16:43:15 +0300

    init commit

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -6,12 +6,7 @@ any_input = input('Enter something: ')
 
 
 def input_to_list(any_input):
-    return ' '.join(any_input).split()
+    return any_input.split()
 
 
 print(input_to_list(any_input))
-
-
-@input_to_list
-def return_input(*args):
-    return args
