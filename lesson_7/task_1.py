b6fbf3f50b7bec228d7fc207792da216efffa7a1
liltar <Freedomforever94>
2020-12-04 20:26:30 +0300

    init commit

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -1,12 +1,16 @@
 # 1. Написать функцию которая вернет строку введенную пользователем.
 #
 #   Обернуть функцию в декоратор чтобы функция вместо строки целиком вернула список слов.
-
+#
 any_input = input('Enter something: ')
 
 
-def input_to_list(any_input):
-    return any_input.split()
+def input_to_list(input_return):
+    return input_return.split()
+
+
+def input_return():
+        return any_input
 
 
 print(input_to_list(any_input))
