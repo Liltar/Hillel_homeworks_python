8f33246d57ce365ff32728f04117ea6a474b423d
liltar <Freedomforever94>
2020-12-07 15:26:34 +0300

    init commit

--- lesson_7/task_2.py
+++ lesson_7/task_2.py
@@ -10,21 +10,21 @@ exit_text = 'The temperature is {} in Celsius, {} in Fahreinheits, {} in Kelvins
 
 
 def celsius():
-        fahr = int(degrees * 9/5 + 32.0)
-        kelv = degrees + 273
-        return exit_text.format(degrees, fahr, kelv)
+    fahr = int(degrees * 9 / 5 + 32.0)
+    kelv = degrees + 273
+    return exit_text.format(degrees, fahr, kelv)
 
 
 def fahreinheits():
-        cels = (degrees - 32) * 5/9
-        kelv = cels + 273
-        return exit_text.format(cels, degrees, kelv)
+    cels = (degrees - 32) * 5 / 9
+    kelv = cels + 273
+    return exit_text.format(cels, degrees, kelv)
 
 
 def kelvins():
-        cels = degrees - 273
-        fahr = cels * 9/5 + 32
-        return exit_text.format(cels, fahr, degrees)
+    cels = degrees - 273
+    fahr = cels * 9 / 5 + 32
+    return exit_text.format(cels, fahr, degrees)
 
 
 def temp_calc():
