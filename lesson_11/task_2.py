941f45eb9d10298984fc2bcc8a500a235c4b1d02
liltar <Freedomforever94>
2020-12-15 02:18:16 +0300

    init commit

--- lesson_11/task_2.py
+++ lesson_11/task_2.py
@@ -1,22 +1,24 @@
 # 2. Написать функцию которая будет частично скрывать e-mail, пример:
 #
 #   hide_email(somebody_email@gmail.com) -> em***@**il.com
-
 #
-def hide_email(str):
-    index_of_at = [pos for pos, char in enumerate(str) if char == '@'][0]
-    return str[index_of_at - 3:index_of_at], str[index_of_at + 1:index_of_at + 3]
-#
-#
-# print(hide_email('somebody_email@gmail.com'))
+def hide_email(some_string):
+    index_of_at = [pos for pos, char in enumerate(some_string) if char == '@'][0]
+    some_string = list(some_string)
+    some_string[index_of_at - 3:index_of_at] = '*'
+    some_string[index_of_at + 1:index_of_at + 3] = '*'
+    return some_string
 
 
-# def hide_email_2(str):
-#     index_of_at = [pos for pos, char in enumerate(str) if char == '@'][0]
-#     str_list = list(str)
-#     str_list[index_of_at - 3:index_of_at] = '*'
-#     str_list_new = ''.join(str_list)
-#     return str_list_new
+print(hide_email('somebody_email@gmail.com'))
+
+
+# def hide_email_2(some_string):
+#     index_of_at = [pos for pos, char in enumerate(some_string) if char == '@'][0]
+#     some_string_list = list(some_string)
+#     some_string_list[index_of_at - 3:index_of_at] = '*'
+#     some_string_list_new = ''.join(some_string_list)
+#     return some_string_list_new
 
 
 # print(hide_email_2('somebody_email@gmail.com'))
