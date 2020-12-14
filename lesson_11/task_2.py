da32bbe7b41121d51b2ce6801eb78df9573efe60
liltar <Freedomforever94>
2020-12-15 02:42:15 +0300

    init commit

--- lesson_11/task_2.py
+++ lesson_11/task_2.py
@@ -5,20 +5,10 @@
 def hide_email(some_string):
     index_of_at = [pos for pos, char in enumerate(some_string) if char == '@'][0]
     some_string = list(some_string)
-    some_string[index_of_at - 3:index_of_at] = '*'
-    some_string[index_of_at + 1:index_of_at + 3] = '*'
-    return some_string
+    some_string[index_of_at - 3:index_of_at] = '***'
+    some_string[index_of_at + 1:index_of_at + 3] = '**'
+    new_string = ''.join(some_string)
+    return new_string
 
 
 print(hide_email('somebody_email@gmail.com'))
-
-
-# def hide_email_2(some_string):
-#     index_of_at = [pos for pos, char in enumerate(some_string) if char == '@'][0]
-#     some_string_list = list(some_string)
-#     some_string_list[index_of_at - 3:index_of_at] = '*'
-#     some_string_list_new = ''.join(some_string_list)
-#     return some_string_list_new
-
-
-# print(hide_email_2('somebody_email@gmail.com'))
